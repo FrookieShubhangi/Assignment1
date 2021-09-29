ca83c49ee7611acde1200a4643cd7f9a8d42cca7
FrookieShubhangi <91489141+FrookieShubhangi@users.noreply.github.com>
2021-09-29 12:40:49 +0530

    Update AccountOwner.java

--- AccountOwner.java
+++ AccountOwner.java
@@ -1,5 +1,19 @@
 package BankAccountManagementSystem;
 
 public class AccountOwner {
-
+	String ownerName;
+	String accountType;
+	String ownerAddressCity;
+	String ownerAddressState;
+	String ownerAddressPincode;
+	double balanceAmount=0.0;
+	String accountCreationDate;
+	String status;
+	public AccountOwner(String name, String accountType, String date, double amount, String status) {
+		this.ownerName = name;
+		this.accountType = accountType;
+		this.accountCreationDate = date;
+		this.balanceAmount = amount;
+		this.status= status;
+	}
 }
